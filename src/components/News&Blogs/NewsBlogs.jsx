668acffbcdf0f0f35a716e20ae91f106b70d858e
issaabubakry <issaabubakry@gmail.com>
2024-08-18 15:10:24 -0700

    created the blog and news section

---
+++ src/components/News&Blogs/NewsBlogs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewsBlogs = () => {
+  return (
+    <>
+        <div className="ftco-section">
+            <div className="container">
+                
+            </div>
+        </div>
+    </>
+
+  )
+}
+
+export default NewsBlogs
