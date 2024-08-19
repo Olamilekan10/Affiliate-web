8db9461cd51008b60a79a6b9fc0336c642fbca10
issaabubakry <issaabubakry@gmail.com>
2024-08-19 15:34:40 -0700

    skipped the news section to correct errors and move to the footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+// import footerImg from '../../images/bg_2.webp';
+
+const Footer = () => {
+  return (
+    <>
+        <footer className="footer">
+            <div className="container">
+                {/* first row */}
+                <div className="row justify-content-center">
+                    
+                </div>
+            </div>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
