6de00b647f50d9edb36bd323bbba54dee86730d8
issaabubakry <issaabubakry@gmail.com>
2024-08-18 19:27:41 -0700

    created a section with container and some header and content div

--- src/components/News&Blogs/NewsBlogs.jsx
+++ src/components/News&Blogs/NewsBlogs.jsx
@@ -5,7 +5,15 @@ const NewsBlogs = () => {
     <>
         <div className="ftco-section">
             <div className="container">
-                
+                {/* header section */}
+                <div className="row justify-content-center pb-5 mb-3">
+                    <div className="col-md-7 heading-section text-center animate">
+                        <h2>Latest news from our blog</h2>
+                        <span className="subheading">News & Blog</span>
+                    </div>
+                </div>
+                {/* content section */}
+                <div className="row d-flex"></div>
             </div>
         </div>
     </>
