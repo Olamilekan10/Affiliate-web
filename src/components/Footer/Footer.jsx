607ed3c79d9c76dc455c008542aa8322f4dd2f87
issaabubakry <issaabubakry@gmail.com>
2024-08-19 15:51:41 -0700

    created a form in the col section and styled it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,12 +5,18 @@ import './Footer.css';
 const Footer = () => {
   return (
     <>
-        <footer className="footer">
+        <footer className="footer-section">
             <div className="container">
                 {/* first row */}
                 <div className="row justify-content-center">
                     <div className="col-10 col-lg-6">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus itaque nam vitae. Consequatur vel quibusdam dolor aspernatur! Ipsa corporis nam a dolor nemo, architecto aliquid facilis impedit harum, repudiandae ratione!
+                        <div className="subscribe mb-5">
+                            <form action="#" className="subscribe-form">
+                                <div className="form-group-d-flex">
+                                    
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 {/* second row */}
