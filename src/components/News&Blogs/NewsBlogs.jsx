af881be0c85363dee49016f5c5a81a0679c2717e
issaabubakry <issaabubakry@gmail.com>
2024-08-19 15:24:07 -0700

    corrected the wrong code written for the news section

--- src/components/News&Blogs/NewsBlogs.jsx
+++ src/components/News&Blogs/NewsBlogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NewsBlogs.css';
-import newImg1 from '../../images/healthy1.jpg';
+// import newImg1 from '../../images/healthy1.jpg';
 
 const NewsBlogs = () => {
   return (
@@ -15,57 +15,34 @@ const NewsBlogs = () => {
                     </div>
                 </div>
                 {/* content section */}
-                {/* <div className="row d-flex">
-                    <div className="col-md-4 d-flex animate">
-                        <div className="blog-entry align-self-stretch">
-                            <a href="/" className="block-20 rounded" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus laudantium alias impedit incidunt quibusdam. Ipsam quidem eligendi asperiores, nemo porro fugiat aliquid laborum exercitationem, eaque ipsa aspernatur dolores quam.</a>
-                        </div>
-                    </div>
-                    <div className="col-md-4 d-flex animate">
-                        <div className="blog-entry align-self-stretch">
-                            <a href="/" className="block-20 rounded">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit veniam possimus hic. Quam labore, cumque obcaecati iste ducimus voluptates dicta exercitationem eveniet. Reprehenderit amet nam nobis, assumenda veritatis sapiente voluptatum.
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-md-4 d-flex animate">
-                        <div className="blog-entry align-self-stretch">
-                            <a href="/" className="block-20 rounded">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, illo? Consectetur aut cum quos. Consequuntur, laboriosam officiis alias modi, quaerat cupiditate voluptatem totam, hic recusandae in temporibus accusamus maiores a.</a>
-                        </div>
-                    </div>
-                </div> */}
 
-                <div class="card-deck p-5">
-                <div class="card">
-                    <img src={newImg1} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="row d-flex">
+                    <div className="col-md-4 d-flex animate">
+                        <div className="blog-entry align-self-stretch">
+                            <a href="/" className="block-20 rounded" ></a>
+                            <div className="text-muted">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia impedit ipsum dicta necessitatibus nostrum, consectetur itaque ullam sunt minima, id non vero amet? Illum id blanditiis exercitationem sed animi quisquam!
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <div className="col-md-4 d-flex animate">
+                        <div className="blog-entry align-self-stretch">
+                            <a href="/" className="block-20 rounded"></a>
+                            <div className="text-muted">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia impedit ipsum dicta necessitatibus nostrum, consectetur itaque ullam sunt minima, id non vero amet? Illum id blanditiis exercitationem sed animi quisquam!
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="card">
-                    <img src={newImg1} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src={newImg1} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    </div>
-                    <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <div className="col-md-4 d-flex animate">
+                        <div className="blog-entry align-self-stretch">
+                            <a href="/" className="block-20 rounded"></a>
+                            <div className="text-muted">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia impedit ipsum dicta necessitatibus nostrum, consectetur itaque ullam sunt minima, id non vero amet? Illum id blanditiis exercitationem sed animi quisquam!
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
+
             </div>
         </div>
     </>
