3d91b6aaae377ef1f69ab367b13b35cb3260a854
issaabubakry <issaabubakry@gmail.com>
2024-08-19 23:06:38 -0700

    styled the input type submit and created more content on the second

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,14 +14,29 @@ const Footer = () => {
                             <form action="#" className="subscribe-form">
                                 <div className="form-group d-flex">
                                     <input type="text" className="form-control rounded-left" placeholder="Enter email address" />
-                                    <input type="submit" value="Subscribe" className="form-control submit px-3" placeholder="Enter email address" />
+                                    <input type="submit" value="Subscribe" className="form-control submit px-3" />
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
                 {/* second row */}
-                <div className="row"></div>
+                <div className="row">
+                    <div className="col-md-4 col-lg-5">
+                        <div className="row">
+                            <div className="col-md-12 col-lg-8 mb-md-0 mb-4">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-8 col-lg-7">
+                        <div className="row">
+                            {/* <div className="col-md-12 col-lg-8 mb-md-0 mb-4">
+
+                            </div> */}
+                        </div>
+                    </div>
+                </div>
                 {/* third row */}
                 <div className="row mt-5"></div>
             </div>
